62c8cdeb499090e1b63b13b70d8a27347185aa85
ilberr <ilhbrilhbr@.com>
2022-02-06 02:12:58 +0100

    crud admin

--- Angular/src/app/user/admin/admin.component.ts
+++ Angular/src/app/user/admin/admin.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { User } from 'src/app/model/user.model';
 import { AdminService } from 'src/app/service/admin.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -15,7 +16,9 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class AdminComponent implements OnInit {
 
   constructor(public apiService: ApiService,public Adminservice: AdminService,private router: Router) { }
-  public users: User[];
+  public users: User[] ;
+  public user: User= <User>{};
+  public edituser: User;
 
   ngOnInit(): void {
     this.getUsers();
@@ -23,7 +26,7 @@ export class AdminComponent implements OnInit {
 
   public getUsers(): void{
     this.Adminservice.getUsers().subscribe(
-      (response: User[]) => {
+      (response: User[])  => {
         this.users = response;
       },
       (error: HttpErrorResponse) => {
@@ -31,6 +34,49 @@ export class AdminComponent implements OnInit {
       }
       
     );
+  }
+  public editUser(user: User): void {
+    this.Adminservice.updateUser(user).subscribe(
+      (response: User) => {
+        console.log(response);
+        this.getUsers();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+  public addUser(addForm: NgForm): void {
+    document.getElementById('add-user-form')!.click();
+    this.Adminservice.addUser(addForm.value).subscribe(
+      (response: User) => {
+        console.log(response);
+        this.getUsers();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+
+  public onOpenModal(user: User= <User>{}, mode: string): void {
+    const container = document.getElementById('main');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addUserModal');
+    }
+    if (mode === 'edit') {
+      this.edituser = user;
+      button.setAttribute('data-target', '#updateUserModal');
+    }
+    container!.appendChild(button);
+    button.click();
   }
 
   
@@ -52,25 +98,4 @@ export class AdminComponent implements OnInit {
   printPDF(user:any){
     console.log("printPDF")
   }}
-  /*
-  users: any[] = [
-    {
-      userId: 1,
-      name: 'ilham',
-      project: 'proj1',
-      start: format(startOfDay(new Date()), 'yyyy-MM-dd'),
-      end: format(endOfDay(new Date()), 'yyyy-MM-dd'),
-      period: '1 months',
-      manager: 'Jack'
-    },
-    {
-      userId: 2,
-      name: 'ilber',
-      project: 'proj2',
-      start: format(startOfDay(new Date()), 'yyyy-MM-dd'),
-      end: format(endOfDay(new Date()), 'yyyy-MM-dd'),
-      period: '1 months',
-      manager: 'Jack'
-    },
-];
-}*/
+  
